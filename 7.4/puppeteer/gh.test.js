f770a7d6ecd75222f0365d3bff5091dc7490f829
Алексей Александров <Alex37v2@gmail.com>
2023-06-30 13:37:55 +1000

    ready

--- 7.4/puppeteer/gh.test.js
+++ 7.4/puppeteer/gh.test.js
@@ -1,71 +1,72 @@
 let page;
 
-afterEach(() => {
-  // после каждого теста
-  page.close(); // вкладка закрывается
-  // browser.close(); // браузер закрывается
-});
-
 describe("Github page tests", () => {
-  // тесты страницы Github
   beforeEach(async () => {
-    // перед каждым тестом
-    page = await browser.newPage(); // открыввется новая вкладка в браузере
-    await page.goto("https://github.com/team"); // загружается страница с адресом...
+    page = await browser.newPage();
+    await page.goto("https://github.com/team");
   });
+
+  afterEach(() => {
+    page.close();
+  });
+
   test("The h1 header content'", async () => {
-    // тестируем содержимое заголовка с тегом h1
-    await page.setDefaultTimeout(2000); // устанавливаем таймаут для увеличения время выполнения каждого шага теста
     const firstLink = await page.$("header div div a");
     await firstLink.click();
     await page.waitForSelector("h1");
     const title2 = await page.title();
-    expect(title2).toEqual("GitHub: Where the world builds software · GitHub");
+    expect(title2).toEqual(
+      "GitHub for teams · Build like the best teams on the planet · GitHub"
+    );
   });
 
   test("The first link attribute", async () => {
-    await page.setDefaultTimeout(2000);
     const actual = await page.$eval("a", (link) => link.getAttribute("href"));
     expect(actual).toEqual("#start-of-content");
   });
 
   test("The page contains Sign in button", async () => {
-    await page.setDefaultTimeout(2000);
     const btnSelector = ".btn-large-mktg.btn-mktg";
     await page.waitForSelector(btnSelector, {
       visible: true,
     });
     const actual = await page.$eval(btnSelector, (link) => link.textContent);
-    expect(actual).toContain("Sign up for free");
+    expect(actual).toContain("Get started with Team");
   });
 });
-describe("Explore page tests2", () => {
+
+describe("GitHub titles tests", () => {
   beforeEach(async () => {
     page = await browser.newPage();
-    await page.goto("https://github.com/explore");
+    await page.goto("https://github.com/");
   });
-  test("The h2 header content Explore", async () => {
-    await page.setDefaultTimeout(3000);
-    const actual = await page.$eval(
-      "div.site-subnav div > a:nth-child(2)",
-      (text) => text.textContent
-    ); // textContent возвращает то что содержится по селоктору в виде текста и сохраняет в переменную
-    expect(actual).toContain("Explore");
+
+  afterEach(() => {
+    page.close();
   });
-  test("The h2 header content Topics", async () => {
-    await page.setDefaultTimeout(3000);
-    const actual = await page.$eval(
-      "div.site-subnav div > a:nth-child(3)",
-      (text) => text.textContent
-    ); // textContent возвращает то что содержится по селоктору в виде текста и сохраняет в переменную
-    expect(actual).toContain("Topics");
+
+  test("Sign in page title test", async () => {
+    await page.click(".btn-mktg.width-full");
+    await page.waitForTimeout(1000);
+    const title = await page.title();
+    expect(title).toEqual("Join GitHub · GitHub");
   });
-  test("The h2 header content Trending", async () => {
-    await page.setDefaultTimeout(3000);
-    const actual = await page.$eval(
-      "div.site-subnav div > a:nth-child(4)",
-      (text) => text.textContent
-    ); // textContent возвращает то что содержится по селоктору в виде текста и сохраняет в переменную
-    expect(actual).toContain("Trending");
+
+  test("Pricing page title test", async () => {
+    await page.click(
+      "body > div.logged-out.env-production.page-responsive.header-overlay.home-campaign > div.application-main > main > div:nth-child(1) > div.px-3.home-campaign-hero > div > div > div.col-11.text-left.pt-12.mt-12.pl-2.pl-sm-0 > div.d-flex.flex-column.flex-md-row > a"
+    );
+    await page.waitForTimeout(1000);
+    const title = await page.title();
+    expect(title).toEqual("Choose an Enterprise plan · GitHub");
+  });
+
+  test("Sponsors page title test", async () => {
+    await page.click(
+      "#collaboration > div > div.overflow-hidden.rounded-3.mb-3.mb-md-5.home-skew.js-home-skew > div > div.d-md-flex.flex-column.flex-1.p-5.p-sm-6.py-lg-8.pl-lg-8.pr-lg-12.col-md-6.flex-justify-between > div > a"
+    );
+    await page.waitForTimeout(1000);
+    const title = await page.title();
+    expect(title).toEqual("GitHub Sponsors · GitHub");
   });
 });
