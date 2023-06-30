cf73560e717374a4b76a1a86e34ba406eaef8944
Алексей Александров <Alex37v2@gmail.com>
2023-06-30 16:28:00 +1000

    fixes

--- 7.4/puppeteer/gh.test.js
+++ 7.4/puppeteer/gh.test.js
@@ -1,31 +1,35 @@
 let page;
+let timeout = 1000;
 
 describe("Github page tests", () => {
-  beforeEach(async () => {
+  beforeAll(async () => {
     page = await browser.newPage();
+  });
+
+  beforeEach(async () => {
     await page.goto("https://github.com/team");
   });
 
-  afterEach(() => {
-    page.close();
+  afterAll(async () => {
+    await page.close();
   });
 
-  test("The h1 header content'", async () => {
+  it("should have correct h1 header content", async () => {
     const firstLink = await page.$("header div div a");
     await firstLink.click();
-    await page.waitForSelector("h1");
+    await page.waitForSelector("h1", { timeout: 2000 });
     const title2 = await page.title();
     expect(title2).toEqual(
       "GitHub for teams · Build like the best teams on the planet · GitHub"
     );
   });
 
-  test("The first link attribute", async () => {
+  it("should have correct first link attribute", async () => {
     const actual = await page.$eval("a", (link) => link.getAttribute("href"));
     expect(actual).toEqual("#start-of-content");
   });
 
-  test("The page contains Sign in button", async () => {
+  it("should contain Sign in button", async () => {
     const btnSelector = ".btn-large-mktg.btn-mktg";
     await page.waitForSelector(btnSelector, {
       visible: true,
@@ -36,36 +40,41 @@ describe("Github page tests", () => {
 });
 
 describe("GitHub titles tests", () => {
-  beforeEach(async () => {
+  beforeAll(async () => {
     page = await browser.newPage();
+  });
+
+  beforeEach(async () => {
     await page.goto("https://github.com/");
   });
 
-  afterEach(() => {
-    page.close();
+  afterAll(async () => {
+    await page.close();
   });
 
-  test("Sign in page title test", async () => {
-    await page.click(".btn-mktg.width-full");
-    await page.waitForTimeout(1000);
+  it("should have correct Sign in page title", async () => {
+    await page.click(".btn-mktg.width-full", { timeout: 5000 });
+    await page.waitForTimeout(timeout);
     const title = await page.title();
     expect(title).toEqual("Join GitHub · GitHub");
   });
 
-  test("Pricing page title test", async () => {
+  it("should have correct Pricing page title", async () => {
     await page.click(
-      "body > div.logged-out.env-production.page-responsive.header-overlay.home-campaign > div.application-main > main > div:nth-child(1) > div.px-3.home-campaign-hero > div > div > div.col-11.text-left.pt-12.mt-12.pl-2.pl-sm-0 > div.d-flex.flex-column.flex-md-row > a"
+      "body > div.logged-out.env-production.page-responsive.header-overlay.home-campaign > div.application-main > main > div:nth-child(1) > div.px-3.home-campaign-hero > div > div > div.col-11.text-left.pt-12.mt-12.pl-2.pl-sm-0 > div.d-flex.flex-column.flex-md-row > a",
+      { timeout: 6000 }
     );
-    await page.waitForTimeout(1000);
+    await page.waitForTimeout(timeout);
     const title = await page.title();
     expect(title).toEqual("Choose an Enterprise plan · GitHub");
   });
 
-  test("Sponsors page title test", async () => {
+  it("should have correct Sponsors page title", async () => {
     await page.click(
-      "#collaboration > div > div.overflow-hidden.rounded-3.mb-3.mb-md-5.home-skew.js-home-skew > div > div.d-md-flex.flex-column.flex-1.p-5.p-sm-6.py-lg-8.pl-lg-8.pr-lg-12.col-md-6.flex-justify-between > div > a"
+      "#collaboration > div > div.overflow-hidden.rounded-3.mb-3.mb-md-5.home-skew.js-home-skew > div > div.d-md-flex.flex-column.flex-1.p-5.p-sm-6.py-lg-8.pl-lg-8.pr-lg-12.col-md-6.flex-justify-between > div > a",
+      { timeout: 7000 }
     );
-    await page.waitForTimeout(1000);
+    await page.waitForTimeout(timeout);
     const title = await page.title();
     expect(title).toEqual("GitHub Sponsors · GitHub");
   });
